6dfacaa3e2a4cec0a54c6220e6b766e88ddf1436
Fábio Mendes <ms.fabiomendes@gmail.com>
2022-06-01 15:06:06 -0300

    fix unlockable time

--- src/mappings/index.ts
+++ src/mappings/index.ts
@@ -1,6 +1,10 @@
-import { PowerLocked, PowerUnlocked } from '../types/GIVPower/GIVPower';
+import {
+  GIVPower,
+  PowerLocked,
+  PowerUnlocked,
+} from '../types/GIVPower/GIVPower';
 import { PowerLock } from '../types/schema';
-import { ROUND_IN_SEC } from '../utils/constants';
+import { GIVPOWER_ADDRESS } from '../utils/constants';
 import { scaleDown } from '../utils/math';
 import { getUserEntity } from '../utils/misc';
 
@@ -11,18 +15,32 @@ export function handlePowerLocked(event: PowerLocked): void {
   user.givPower = user.givPower.plus(powerAmount);
   user.save();
 
+  let initialDate: i32;
+  let roundDuration: i32;
+
+  const givpower = GIVPower.bind(GIVPOWER_ADDRESS);
+  const initialDateCall = givpower.try_initialDate();
+  if (!initialDateCall.reverted) {
+    initialDate = initialDateCall.value.toI32();
+  }
+  const roundDurationCall = givpower.try_roundDuration();
+  if (!initialDateCall.reverted) {
+    roundDuration = roundDurationCall.value.toI32();
+  }
+
   const txHash = event.transaction.hash.toHex();
   const rounds = event.params.rounds.toI32();
   const untilRound = event.params.untilRound.toI32();
-  const unlockableAt = event.block.timestamp.toI32() + rounds * ROUND_IN_SEC;
 
   const powerLock = new PowerLock(txHash);
   powerLock.user = userAddress.toHex();
   powerLock.amount = powerAmount;
   powerLock.untilRound = untilRound;
-  powerLock.unlockableAt = unlockableAt;
   powerLock.rounds = rounds;
-  powerLock.locked = true;
+  powerLock.unlocked = false;
+  if (initialDate && roundDuration) {
+    powerLock.unlockableAt = initialDate + untilRound * roundDuration;
+  }
   powerLock.save();
 }
 
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,9 @@
-import { BigDecimal } from '@graphprotocol/graph-ts';
+import { Address, BigDecimal } from '@graphprotocol/graph-ts';
 
 export const ROUND_IN_SEC = 5 * 60;
 
 export const ZERO_BD = BigDecimal.fromString('0');
+
+export const GIVPOWER_ADDRESS = Address.fromString(
+  '0xdA8576b68501540D6e5C4e5BbB644447541Ff628',
+);
