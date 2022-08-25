9e2a15c15f00511f312e814493bd5f172caacc37
Mateo Daza <mateodaza@gmail.com>
2022-08-25 15:08:53 -0500

    forces manifest generation

--- src/mappings/uniswapV3/uniswapV3PositionsNftMapping.ts
+++ src/mappings/uniswapV3/uniswapV3PositionsNftMapping.ts
@@ -1,6 +1,6 @@
 import {
   IncreaseLiquidity,
-  // UniswapV3PositionsNFT,
+  UniswapV3PositionsNFT,
   Transfer,
   DecreaseLiquidity,
 } from '../../types/UniswapV3PositionsNFT/UniswapV3PositionsNFT';
@@ -39,56 +39,61 @@ function updateUniswapV3PoolLiquidity(): void {
 }
 
 export function handleIncreaseLiquidity(event: IncreaseLiquidity): void {
-  // const tokenId = event.params.tokenId;
-  // const uniswapToken = UniswapPosition.load(tokenId.toString());
-  // if (uniswapToken) {
-  //   uniswapToken.liquidity = uniswapToken.liquidity.plus(
-  //     event.params.liquidity,
-  //   );
-  //   uniswapToken.closed = false;
-  //   uniswapToken.save();
-  //   recordUniswapV3InfinitePositionReward(event.block.timestamp);
-  //   return;
-  // }
-  // const contract = UniswapV3PositionsNFT.bind(event.address);
-  // const positionsResult = contract.try_positions(tokenId);
-  // if (positionsResult.reverted) {
-  //   return;
-  // }
-  // const positions = positionsResult.value;
-  // const token0 = positions.value2.toHex();
-  // const token1 = positions.value3.toHex();
-  // const isGivEthLiquidity: boolean =
-  //   (token0 == uniswapV3Config.MAINNET_GIV_TOKEN_ADDRESS.toLowerCase() &&
-  //     token1 == uniswapV3Config.MAINNET_WETH_TOKEN_ADDRESS.toLowerCase()) ||
-  //   (token0 == uniswapV3Config.MAINNET_WETH_TOKEN_ADDRESS.toLowerCase() &&
-  //     token1 == uniswapV3Config.MAINNET_GIV_TOKEN_ADDRESS.toLowerCase());
-  // //value4 is fee
-  // if (positions.value4 == fee && isGivEthLiquidity == true) {
-  //   const owner = contract.ownerOf(tokenId).toHex();
-  //   const uniswapStakedPosition = new UniswapPosition(tokenId.toString());
-  //   uniswapStakedPosition.tokenId = tokenId.toString();
-  //   uniswapStakedPosition.liquidity = positions.value7;
-  //   uniswapStakedPosition.token0 = token0;
-  //   uniswapStakedPosition.token1 = token1;
-  //   uniswapStakedPosition.tokenURI = contract.tokenURI(tokenId);
-  //   uniswapStakedPosition.tickLower = positions.value5;
-  //   uniswapStakedPosition.tickUpper = positions.value6;
-  //   uniswapStakedPosition.owner = owner;
-  //   uniswapStakedPosition.closed = false;
-  //   uniswapStakedPosition.save();
-  //   if (uniswapV3Config.UNISWAP_INFINITE_POSITION == tokenId.toString()) {
-  //     const uniswapInfinitePosition = new UniswapInfinitePosition(
-  //       tokenId.toString(),
-  //     );
-  //     uniswapInfinitePosition.lastRewardAmount = BigInt.zero();
-  //     uniswapInfinitePosition.lastUpdateTimeStamp = event.block.timestamp;
-  //     uniswapInfinitePosition.save();
-  //   } else {
-  //     recordUniswapV3InfinitePositionReward(event.block.timestamp);
-  //   }
-  //   updateUniswapV3PoolLiquidity();
-  // }
+  const tokenId = event.params.tokenId;
+  const uniswapToken = UniswapPosition.load(tokenId.toString());
+  if (uniswapToken) {
+    uniswapToken.liquidity = uniswapToken.liquidity.plus(
+      event.params.liquidity,
+    );
+    uniswapToken.closed = false;
+    uniswapToken.save();
+
+    recordUniswapV3InfinitePositionReward(event.block.timestamp);
+    return;
+  }
+  const contract = UniswapV3PositionsNFT.bind(event.address);
+  const positionsResult = contract.try_positions(tokenId);
+  if (positionsResult.reverted) {
+    return;
+  }
+  const positions = positionsResult.value;
+  const token0 = positions.value2.toHex();
+  const token1 = positions.value3.toHex();
+
+  const isGivEthLiquidity: boolean =
+    (token0 == uniswapV3Config.MAINNET_GIV_TOKEN_ADDRESS.toLowerCase() &&
+      token1 == uniswapV3Config.MAINNET_WETH_TOKEN_ADDRESS.toLowerCase()) ||
+    (token0 == uniswapV3Config.MAINNET_WETH_TOKEN_ADDRESS.toLowerCase() &&
+      token1 == uniswapV3Config.MAINNET_GIV_TOKEN_ADDRESS.toLowerCase());
+
+  //value4 is fee
+  if (positions.value4 == fee && isGivEthLiquidity == true) {
+    const owner = contract.ownerOf(tokenId).toHex();
+    const uniswapStakedPosition = new UniswapPosition(tokenId.toString());
+    uniswapStakedPosition.tokenId = tokenId.toString();
+    uniswapStakedPosition.liquidity = positions.value7;
+    uniswapStakedPosition.token0 = token0;
+    uniswapStakedPosition.token1 = token1;
+    uniswapStakedPosition.tokenURI = contract.tokenURI(tokenId);
+    uniswapStakedPosition.tickLower = positions.value5;
+    uniswapStakedPosition.tickUpper = positions.value6;
+    uniswapStakedPosition.owner = owner;
+    uniswapStakedPosition.closed = false;
+    uniswapStakedPosition.save();
+
+    if (uniswapV3Config.UNISWAP_INFINITE_POSITION == tokenId.toString()) {
+      const uniswapInfinitePosition = new UniswapInfinitePosition(
+        tokenId.toString(),
+      );
+      uniswapInfinitePosition.lastRewardAmount = BigInt.zero();
+      uniswapInfinitePosition.lastUpdateTimeStamp = event.block.timestamp;
+      uniswapInfinitePosition.save();
+    } else {
+      recordUniswapV3InfinitePositionReward(event.block.timestamp);
+    }
+
+    updateUniswapV3PoolLiquidity();
+  }
 }
 
 export function handleDecreaseLiquidity(event: DecreaseLiquidity): void {
